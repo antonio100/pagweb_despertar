d40e67cc15adce2e9a401ddb786cf8e8be5c61df
kako <kako@kako.cl>
2016-06-20 18:08:06 -0400

    contacto terminado

--- sendemail.php
+++ sendemail.php
@@ -1,9 +1,19 @@
 <?php
-$name       = @trim(stripslashes($_POST['name'])); 
+/*$name       = @trim(stripslashes($_POST['name'])); 
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
 $to   		= 'a.alfaro.avalos@gmail.com';//replace with your email
+*/
+
+
+$name       = @trim(stripslashes($_GET['name'])); 
+$from       = @trim(stripslashes($_GET['email'])); 
+$subject    = @trim(stripslashes($_GET['subject'])); 
+$message    = @trim(stripslashes($_GET['message'])); 
+$to   		= 'a.alfaro.avalos@gmail.com';//replace with your email
+
+
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
@@ -13,6 +23,6 @@ $headers[] = "Reply-To: <{$from}>";
 $headers[] = "Subject: {$subject}";
 $headers[] = "X-Mailer: PHP/".phpversion();
 
-mail($to, $subject, $message, $headers);
-
+mail($to, $subject, $message, implode("\r\n", $headers));
+echo " <script>alert('Formulario enviado exitosamente, le responderemos lo más pronto posible.');location.href ='index.html';</script> ";
 die;
